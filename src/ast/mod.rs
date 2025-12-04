cdbccf1a37ee125fe8b568f2d79146778093b655
aiv2005 <aiv2005@sina.com>
2025-12-04 21:52:33 +0800

    refactor(parser): 移除过时的语法定义并扩展表达式解析
    删除不再使用的语法定义文件，重构 sysy.lalrpop 以支持新的表达式结构，包括一元表达式和基本表达式的解析。同时，更新 AST 结构以包含新的表达式相关类型。

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -23,6 +23,25 @@ pub struct Block{
 }
 
 #[derive(Debug)]
-pub struct Stmt{
-    pub num: i32,
+pub enum Stmt{
+    // pub num: i32,
+    Expr,
 }
+
+#[derive(Debug)]
+pub enum PrimaryExp{
+    Number(IntConst),
+    Expr,
+}
+
+#[derive(Debug)]
+pub enum UnaryExp {
+    PrimaryExp,
+    UnaryOpExp(UnaryOp, UnaryExp),
+}
+
+#[derive(Debug)]
+pub struct  Expr{
+    pub unary_exp: UnaryExp,
+}
+
